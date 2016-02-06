974005320d1113feb87b8084ed1af80ce2ab3bca
ved62 <v.dembskiy@gmail.com>
2016-02-06 17:01:34 +0300

    Initial Commit

---
+++ Reversi GameplayKit SpriteKit/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Reversi GameplayKit SpriteKit
+//
+//  Created by Владислав Эдуардович Дембский on 06.02.16.
+//  Copyright © 2016 Vladislav Dembskiy. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
